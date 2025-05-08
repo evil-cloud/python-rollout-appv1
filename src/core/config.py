8e607225f44fa75ddcf3414c0afa7c2251978632
Jose Reynoso <reynosojose2005@gmail.com>
2025-05-07 23:53:45 -0600

    mié 07 may 2025 23:53:45 CST

--- src/core/config.py
+++ src/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Rollout Service"
+    PROJECT_NAME: str = "GitOps API"
     VERSION: str = "1.0.2"
     ROLLOUT_STRATEGY: str = "v1.0.1"
     EXTERNAL_API_URL: str = "https://jsonplaceholder.typicode.com/todos/1"
